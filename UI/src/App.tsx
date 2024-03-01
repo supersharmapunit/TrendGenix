cfbddedfc6a5d17c462cefa93370ff49ff0999e0
supersharmapunit <punit2000sharma@gmail.com>
2024-03-02 01:07:39 +0530

    initialized UI with shadcn dependency

---
+++ UI/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div >
+      <Button>Bhumro Jhumro</Button>
+    </div>
+  )
+}
+
+export default App
