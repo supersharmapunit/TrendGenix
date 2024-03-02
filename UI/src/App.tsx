4bea2e057e44c8296d8644898d90a097662347cb
Devansh Tripathi <devanshtripathi0310@gmail.com>
2024-03-03 01:57:49 +0530

    Created Search UI and Post Component

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,13 +1,21 @@
-import './App.css'
-import { Button } from "@/components/ui/button"
+import { useState } from "react";
+import "./App.css";
+import Searchwrap from "./components/Searchwrap";
+import Post from "./components/Post";
 
 function App() {
-
+  const [searchData,setSearchData]=useState('');
+  const getSearchData=(data:string)=>{
+       setSearchData(data)
+  }
   return (
-    <div >
-      <Button>Bhumro Jhumro</Button>
+    <div className="main h-screen grid grid-rows-1 gap-x-4 p-1">
+        <div className="post-wrapper row-span-1 flex justify-center">
+          <Post></Post>
+        </div>
+        <Searchwrap sendSearchText={getSearchData}></Searchwrap>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
