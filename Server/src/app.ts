4f42e36fd641fa7c325120e540d2867b96b48ad0
supersharmapunit <punit2000sharma@gmail.com>
2024-03-10 00:23:44 +0530

    basic openai api code completed

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,15 +1,19 @@
+import { SERVER_PORT } from './config/serverConfig';
 import express from 'express';
-const app = express();
-const PORT = 3000;
+import { chatAI } from './services/chatService';
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const app = express();
+const PORT = SERVER_PORT || 3001;
+
+app.get('/', async (req, res) => {
+    const response = await chatAI();
+    res.json(response);
 });
 
 function setupAndStartServer() : void {
     app.listen(PORT, () : void => {
         return console.log(`Express is listening at http://localhost:${PORT}`);
-    });
+});
 }
 
 setupAndStartServer();
